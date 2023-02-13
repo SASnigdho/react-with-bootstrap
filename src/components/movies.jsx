0b11fe768835527d89d22563153c19da636a1cc4
sasnigdho <salmanahmedims@gmail.com>
2023-02-14 01:19:25 +0600

    Movie delete function added.

---
+++ src/components/movies.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    count: 0,
+    movies: getMovies(),
+  };
+
+  getValue() {
+    const { movies } = this.state;
+
+    return movies.length === 0
+      ? "ZERO!"
+      : `${movies.length} Movies in Database.`;
+  }
+
+  onDelete = (movie) => {
+    const newMoviees = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies: newMoviees });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>{this.getValue()}</h1>
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Genre</th>
+              <th scope="col">In Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie, index) => (
+              <tr key={movie._id}>
+                <th scope="row">{index + 1}</th>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.onDelete(movie)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
-import Counter from './counter.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>
 );
 
